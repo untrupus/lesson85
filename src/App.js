3ee99f01651da21bad05cbc3abc742cafc9a18e0
Ivan Volodartsev <untrupus@gmail.com>
2020-11-11 22:45:19 +0600

    create app structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+          <Switch>
+            {/*<Route path="/" exact component={Posts}/>*/}
+            {/*<Route path="/news/add" exact component={AddNews}/>*/}
+            {/*<Route path="/news/:id" component={NewsPage}/>*/}
+            <Route render={() => <h1>404</h1>}/>
+          </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="container">
+                <h1>last.fm</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
